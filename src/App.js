bf0a51801df98bf6f206b853846363c239e905b0
Piyush <piyushsagar1120@gmail.com>
2021-10-15 18:42:50 +0530

    Tesla-Clone

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Components/Header.js";
+import Home from "./Components/Home.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
